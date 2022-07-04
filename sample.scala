402aa0efe8dcad284db850024a0f0a200366e3c6
pavel lysianok <Pavel_Lysianok@epam.com>
2022-07-04 18:23:08 +0400

    Fix all

--- sample.scala
+++ sample.scala
@@ -6,10 +6,10 @@ package org.scalafmt
 
            def toEither: Either[Throwable, String] = this match {
                   case Formatted.Success(s) => Right(s)
-          case Formatted.Failure(e) => Left(e)      // too much space before comment
+         case Formatted.Failure(e) => Left(e)      // too much space before comment
                        }
 
-                    def get: String = this match {
+                       def get: String = this match {
             case Formatted.Success(code) => code
         case Formatted.Failure(e) => throw e
   }
