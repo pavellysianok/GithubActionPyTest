e4431b84b21d1d1cc91b568ecdd1611f086d6c9a
pavel lysianok <Pavel_Lysianok@epam.com>
2022-07-04 15:16:39 +0400

    Add diff method

--- sample.scala
+++ sample.scala
@@ -4,13 +4,13 @@ package org.scalafmt
 
 sealed abstract class Formatted {
 
-  def toEither: Either[Throwable, String] = this match {
+    def toEither: Either[Throwable, String] = this match {
       case Formatted.Success(s) => Right(s)
       case Formatted.Failure(e) => Left(e)      // too much space before comment
-  }
+   }
 
   def get: String = this match {
-    case Formatted.Success(code) => code
+      case Formatted.Success(code) => code
     case Formatted.Failure(e) => throw e
   }
 }
