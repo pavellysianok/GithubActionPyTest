50b245cea1408d090c58b5bb1a975303603dbe61
pavel lysianok <Pavel_Lysianok@epam.com>
2022-07-04 18:33:46 +0400

    Test again

--- sample.scala
+++ sample.scala
@@ -6,7 +6,7 @@ package org.scalafmt
 
            def toEither: Either[Throwable, String] = this match {
                   case Formatted.Success(s) => Right(s)
-         case Formatted.Failure(e) => Left(e)      // too much space before comment
+       case Formatted.Failure(e) => Left(e)      // too much space before comment
                  }
 tt
                        def get: String = this match {
@@ -20,5 +20,5 @@ tt
  */
                    object Formatted                        {
          case class Success(formattedCode: String) extends Formatted
-                   case class Failure(e: Throwable) extends Formatted
+              case class Failure(e: Throwable) extends Formatted
              }
