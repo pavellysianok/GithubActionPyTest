414227b3e04394fd968c7f4a4233dfba49d5bc01
pavel lysianok <Pavel_Lysianok@epam.com>
2022-07-04 16:12:05 +0400

    Mode changed for PR

--- sample.scala
+++ sample.scala
@@ -7,15 +7,15 @@ package org.scalafmt
             def toEither: Either[Throwable, String] = this match {
                    case Formatted.Success(s) => Right(s)
               case Formatted.Failure(e) => Left(e)      // too much space before comment
-          }
+                        }
 
-               def get: String = this match {
+                    def get: String = this match {
       case Formatted.Success(code) => code
         case Formatted.Failure(e) => throw e
   }
 }
 
-/** Aligned by first asterisk, default ScalaDoc style is second.
+/** Al  igned by first asterisk, default ScalaDoc style is second.
  *
  */
                                          object Formatted                        {
