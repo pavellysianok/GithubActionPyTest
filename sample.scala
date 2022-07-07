468f7b9f348afcc96f8a79a4bf68a5a3f8e5dd2f
pavel lysianok <Pavel_Lysianok@epam.com>
2022-07-07 17:43:52 +0400

    Add two files fro checking

--- sample.scala
+++ sample.scala
@@ -6,10 +6,9 @@ package org.scalafmt
 
       def toEither: Either[Throwable, String] = this match {
                case Formatted.Success(s) => Right(s)
-          case Formatted.Failure(e) => Left(e)      // too much space before comment
+         case Formatted.Failure(e) => Left(e)      // too much space before comment
                  }
-tt
-                  def get: String = this match {
+tt                  def get: String = this match {
                        case Formatted.Success(code) => code
             case Formatted.Failure(e) => throw e
   }
@@ -19,6 +18,6 @@ tt
  *
  */
                    object Formatted                        {
-              case class Success(formattedCode: String) extends Formatted
+            case class Success(formattedCode: String) extends Formatted
             case class Failure(e: Throwable) extends Formatted
              }
