2c15be8eacd2ddd1641af8d1818cefa2b96c29f9
pavel lysianok <Pavel_Lysianok@epam.com>
2022-07-08 15:06:46 +0400

    Fix path

--- sample copy.scala
+++ sample copy.scala
@@ -5,10 +5,10 @@ package org.scalafmt
     sealed abstract class Formatted {
 
       def toEither: Either[Throwable, String] = this match {
-            case Formatted.Success(s) => Right(s)
+             case Formatted.Success(s) => Right(s)
        case Formatted.Failure(e) => Left(e)      // too much space before comment
                  }
-        def get: String = this match {
+          def get: String = this match {
                    case Formatted.Success(code) => code
             case Formatted.Failure(e) => throw e
   }
@@ -18,6 +18,6 @@ package org.scalafmt
  *
  */
              object Formatted                        {
-      case class Success(formattedCode: String) extends Formatted
-      case class Failure(e: Throwable) extends Formatted
+       case class Success(formattedCode: String) extends Formatted
+       case class Failure(e: Throwable) extends Formatted
              }
